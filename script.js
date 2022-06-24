766c8f37d74d071de5f39c193ef778fd3f351fec
astridjjg313 <107772855+astridjjg313@users.noreply.github.com>
2022-06-24 16:20:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    var principal = document.getElementById(principal").value;
+    var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
@@ -6,6 +6,7 @@
 function compute()
 {
         p = document.getElementById("principal").value;
+    
 }
 function updateRate()
 {var rateval = document.getElementById("rate").value;
