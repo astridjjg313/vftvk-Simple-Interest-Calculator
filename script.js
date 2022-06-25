458e8ef4824a57845a0df26e492351221930549e
astridjjg313 <107772855+astridjjg313@users.noreply.github.com>
2022-06-25 12:47:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal*years*rate/100;
-    var year = new Date().getFullYear()+parseInt(years);
-function compute()
-{
-        p = document.getElementById("principal").value;
-    
-}
 function updateRate()
-{var rateval = document.getElementById("rate").value;
+{var rateval = document.getElementById("rate").value + '%';
 document.getElementById("rate_val").innerText=rateval;
 }
         
+function compute()
+{   var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit <mark>"
+   + principal + "</mark>, <br/> at an interest rate of <mark>"
+   + rate +"%.</mark><br/> You will receive an amount of <mark>" + interest +
+   "</mark>,<br/>in the year <mark>" + year + ".</mark><br/>";    
+  
+}
+
+function fieldValidation() 
+{
+    var principal = parseFloat(document.getElementById("principal").value);
+    if (principal <= 0) 
+    {
+        alert("Enter a non-zero or a positive number");
+        document.getElementById("principal").focus();
+    }
+
+
+}
